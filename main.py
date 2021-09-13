1073559b8ab6332db9c6abef05f7428fc53afa4b
tjasaa9 <tjasa9trost9@gmail.com>
2021-09-13 22:31:29 +0200

    cookies

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from types import MethodDescriptorType
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import datetime
 
 app = Flask(__name__)
@@ -17,7 +16,11 @@ def index():
 @app.route("/about-me", methods=["GET", "POST"])
 def about():
     if request.method == "GET":
-        return render_template("about.html")
+        user_name = request.cookies.get("user_name")
+        print(user_name)
+
+        return render_template("about.html", name=user_name)
+
     elif request.method == "POST":
         contact_name = request.form.get("contact-name")
         contact_email = request.form.get("contact-email")
@@ -27,7 +30,10 @@ def about():
         print(contact_email)
         print(contact_message)
 
-        return render_template("success.html")
+        response = make_response(render_template("success.html"))
+        response.set_cookie("user_name", contact_name)
+
+        return response
 
     if __name__ == '__main__':
         app.run(use_reloader=True)
