f709610aa069b5a5659316d5248e6aea1babafe7
tjasaa9 <tjasa9trost9@gmail.com>
2021-09-13 21:22:03 +0200

    changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from types import MethodDescriptorType
+from flask import Flask, render_template, request
 import datetime
 
 app = Flask(__name__)
@@ -13,9 +14,20 @@ def index():
 
     return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
 
-@app.route("/about-me")
+@app.route("/about-me", methods=["GET", "POST"])
 def about():
-    return render_template("about.html")
+    if request.method == "GET":
+        return render_template("about.html")
+    elif request.method == "POST":
+        contact_name = request.form.get("contact-name")
+        contact_email = request.form.get("contact-email")
+        contact_message = request.form.get("contact-message")
 
-if __name__ == '__main__':
-    app.run(use_reloader=True)
+        print(contact_name)
+        print(contact_email)
+        print(contact_message)
+
+        return render_template("success.html")
+
+    if __name__ == '__main__':
+        app.run(use_reloader=True)
